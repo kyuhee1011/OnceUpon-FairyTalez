8b45bee12c3055baa1ad06c877d145f7b7835862
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-05-07 05:13:45 -0400

    Set up function for eventlistener, but need CSS.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 const disneyChar =[];
+
 const searchBar=document.querySelector('#search-bar');
+const buttons =document.querySelectorAll('.filterChar')
+
+const films=document.querySelectorAll ('.films-desc')
+const imgFilters=document.querySelectorAll('.anime-char');
 
 
 
-// fetch ('http://localhost:3000/characters')
-// .then ((res) => res.json())
-// .then ((responseCharacters)  =>{responseCharacters.forEach((element) => {
-//     disneyChar.push (element)
-//     renderChar(element)
-// });
-// });
-fetch('http://localhost:3000/characters')
-  .then(res => res.json())
-  .then(disneyChar => {
+fetch ('http://localhost:3000/characters')
+.then ((res) => res.json())
+.then ((responseCharacters)  =>{responseCharacters.forEach((element) => {
+    disneyChar.push (element)
+    renderChar(element)
+});
+});
 
-    const filteredCharacters = disneyChar.filter(char => char.characterType !== "princess");
-    // Loop through the filtered character list and render them
-    filteredCharacters.forEach(char => renderChar(char));
-
-    console.log(filteredCharacters);
-
-  });
-
-// adding the renderChar() function is implemented 
+// The renderChar() function is implemented as before
 function renderChar (character) {
-    const animeChar =document.createElement ("div")
-    animeChar.classList.add ("anime-char")
-// adding another class to each div
- let charClass = "";
-  switch (character.characterType) {
+
+const animeChar =document.createElement ("div")
+animeChar.classList.add ("anime-char")
+
+let charClass = "";
+switch (character.characterType) {
   case "prince":
     charClass = "prince-char";
     break;
@@ -45,49 +40,66 @@ function renderChar (character) {
     charClass = "animal-char";
     break;
 }
-  
+
+
 animeChar.classList.add(charClass);
 
-    const h2 =document.createElement ("h2");
-    h2.textContent =character.name;
 
-    const  img =document.createElement ("img");
-    img.src=character.imageUrl;
-    img.classList.add ("disney-image");
+const h2 =document.createElement ("h2");
+h2.textContent =character.name;
 
-    const  p =document.createElement("p");
-    p.textContent = character.films;
-    p.classList.add ("films-desc")
-    
-    const imgChar= document.querySelector (".imgChar").appendChild (animeChar)
-    imgChar.append (h2, img, p)
+const  img =document.createElement ("img");
+img.src=character.imageUrl;
+img.classList.add ("disney-image");
 
-    }
+const  p =document.createElement("p");
+p.textContent = character.films;
+p.classList.add ("films-desc")
+;
+const imgChar= document.querySelector (".imgChar").appendChild (animeChar)
+imgChar.append (h2, img, p)
+
+}
+
 
 //search bar event
 
 searchBar.addEventListener("keydown", (e) => {
-console.log (`key=${e.key}, code =${e.code}`);
-});
-
-
-
+    console.log (`key=${e.key}, code =${e.code}`);
+    });
+    
 //button click event
-// const buttons =document.querySelectorAll('.filterChar')
-
-// const films=document.querySelectorAll ('.films-desc')
-// const imgFilters=document.querySelectorAll('.anime-char');
-
-buttonsFilters.forEach ((button)=> {
-    button.addEventListener ("click", (e)=> {
-        e.preventDefault ();
-        setClickBtn (e);
-        const imgBtn =e.target.dataset.filter
-        filterButton(imgBtn);       
-    })
-})
+// buttonsFilters.forEach ((button)=> {
+//     button.addEventListener ("click", (e)=> {
+//         e.preventDefault ();
+//         setClickBtn (e);
+//         const imgBtn =e.target.dataset.filter
+//         filterButton(imgBtn);       
+//     })
+// })
 
 //onclick function
 
+function filterButton(filterImage) {
+    const disneyImg = document.getElementsByClassName("anime-char");
+    
 
+    for (let i = 0; i < disneyImg.length; i++){
+    if (filterImage === "all"|| disneyImg[i].dataset.characterType === filterImage) 
+    {
+        disneyImg[i].classList.remove("hide");
+        disneyImg[i].classList.add("show");
+      }
+    else {
+          disneyImg[i].classList.remove("show");
+          disneyImg[i].classList.add("hide");
+        }
+    }
+}
 
+function  setClickBtn(e){
+  buttonsFilters.forEach((button)=>{
+    button.classList.remove("active");
+  });
+  e.target.classList.add("active");
+}
