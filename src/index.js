cddecb861e30ed64c04de61280c56ff6c5b4e87a
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-05-07 02:51:39 -0400

    Calls data from db.json using the fetch ()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log ("worked")
+const disneyChar =[];
+
+fetch ('http://localhost:3000/characters')
+.then ((res) => res.json())
+.then ((responseCharacters)  =>{responseCharacters.forEach((element) => {
+    disneyChar.push (element)
+ console.log (disneyChar)
+});
+});
