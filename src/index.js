f56a7604bc2d3eec1729b40baa2757dfe06f8028
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-05-07 03:16:30 -0400

    Adds div and gives a class for each character

--- src/index.js
+++ src/index.js
@@ -11,18 +11,21 @@ fetch ('http://localhost:3000/characters')
 // adding the renderChar() function is implemented 
 function renderChar (character) {
     const animeChar =document.createElement ("div")
- 
+    animeChar.classList.add ("anime-char")
+    
         
     const h2 =document.createElement ("h2");
     h2.textContent =character.name;
 
     const  img =document.createElement ("img");
     img.src=character.imageUrl;
+    img.classList.add ("disney-image");
 
     const  p =document.createElement("p");
     p.textContent = character.films;
+    p.classList.add ("films-desc")
     
-    const imgChar= document.querySelector (".imgChar")
+    const imgChar= document.querySelector (".imgChar").appendChild (animeChar)
     imgChar.append (h2, img, p)
 
     }
